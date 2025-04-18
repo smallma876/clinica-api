7c99572e725b0e5b60a64c745a0aee9775d093f8
Sergio Mallma <sergio.mallma876@gmail.com>
2025-04-18 15:39:34 -0500

    remove environment variables

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,11 +2,11 @@ import mysql, { ConnectionOptions } from 'mysql2/promise';
 import { BD_PORT, DATABASE, HOST, PASSWORD, USER } from '../globals';
 
 const config: ConnectionOptions = {
-    host: HOST,
-    user: USER,
-    port: BD_PORT,
-    password: PASSWORD,
-    database: DATABASE,
+    host: HOST || 'localhost',
+    user: USER || 'root',
+    port: BD_PORT || 3306,
+    password: PASSWORD || 'root',
+    database: DATABASE || 'bd_clinica',
 };
 
 export const getConnection = async (): Promise<mysql.Connection> => {
