f73104f3bfd81823c4207920c27557a5cab528df
Sergio Mallma <sergio.mallma876@gmail.com>
2025-04-19 18:56:40 -0500

    feat: configure cors

--- src/app.ts
+++ src/app.ts
@@ -14,11 +14,21 @@ interface CreateAppProps {
     patientModel: PatientModel;
 }
 
+const allowedOrigins = ['http://localhost:5173', 'https://clinica-authorization-production.up.railway.app/'];
+
 export const createApp = ({ userModel, patientModel }: CreateAppProps) => {
     const app = express();
     app.use(
         cors({
-            origin: 'http://localhost:5173', // Cambia por el dominio real de tu frontend
+            origin: (origin, callback) => {
+                // Permitir peticiones sin origin (como curl o Postman)
+                if (!origin) return callback(null, true);
+                if (allowedOrigins.includes(origin)) {
+                    return callback(null, true);
+                } else {
+                    return callback(new Error('No permitido por CORS'));
+                }
+            }, // Cambia por el dominio real de tu frontend
             credentials: true,
         })
     );
